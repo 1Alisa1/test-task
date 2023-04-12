618cd95155b6219885beaf2979e93b8df97090e3
1Alisa1 <Alisa.dragun@mail.ru>
2023-04-12 13:56:07 +0300

    add go back button

--- src/pages/detailsPage/detailsPage.tsx
+++ src/pages/detailsPage/detailsPage.tsx
@@ -1,13 +1,17 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Chart from "../../components/chart/chart";
 import CurrencyDetailsTable from "../../components/currencyDetailsTable/currencyDetailsTable";
 import styles from "./detailsPage.module.scss";
+import { Button } from "../../components/button/button";
 
 const DetailsPage: React.FC = () => {
   const {currencyId} = useParams();
+  const navigate = useNavigate();
+
   return (
     <div className={styles.container}>
       <div className={styles.title}>Details</div>
+      <Button text="Go back" handleClick={() => navigate(-1)}/>
       <CurrencyDetailsTable currencyId={currencyId ?? ""}/>
       <Chart currencyId={currencyId ?? ""}/>
     </div>  
