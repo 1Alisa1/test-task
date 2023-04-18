da5643e4e32158270e10fa442de9ea055210e578
1Alisa1 <Alisa.dragun@mail.ru>
2023-04-18 18:39:27 +0300

    remake the currency table in the mobile version

--- src/components/currencyTable/currencyTable.tsx
+++ src/components/currencyTable/currencyTable.tsx
@@ -45,68 +45,68 @@ const CurrencyTable: React.FC = () => {
               setPage={setPage}
               nextPage={nextPage}
             />
-            <div className={styles.tableContainer}>
-              <table className={styles.table}>
-                <thead>
-                  <tr>
-                    <th>Rank</th>
-                    <th>Symbol</th>
-                    <th>Name</th>
-                    <th>Avaliable supply</th>
-                    <th>Change in 24h</th>
-                    <th>Avarage price in 24h</th>
-                    <th></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {response &&
-                    response
-                      .slice(firstContentIndex, lastContentIndex)
-                      .map((value) => (
-                        <tr
-                          key={value.id}
-                          onClick={() => navigate(`/details/${value.id}`)}
+            <table className={styles.table}>
+              <thead>
+                <tr>
+                  <th className={styles.mobile}>Rank</th>
+                  <th>Symbol</th>
+                  <th className={styles.mobile}>Name</th>
+                  <th className={styles.mobile}>Avaliable supply</th>
+                  <th>Change in 24h</th>
+                  <th>Avarage price in 24h</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {response &&
+                  response
+                    .slice(firstContentIndex, lastContentIndex)
+                    .map((value) => (
+                      <tr
+                        key={value.id}
+                        onClick={() => navigate(`/details/${value.id}`)}
+                      >
+                        <td className={styles.mobile}>{value.rank}</td>
+                        <td>{value.symbol}</td>
+                        <td className={styles.mobile}>{value.name}</td>
+                        <td className={styles.mobile}>
+                          {Number(value.supply).toFixed(2)}
+                        </td>
+                        <td
+                          style={
+                            value.changePercent24Hr &&
+                            value.changePercent24Hr[0] === "-"
+                              ? { backgroundColor: "#ff00004d" }
+                              : { backgroundColor: "#04760473" }
+                          }
                         >
-                          <td>{value.rank}</td>
-                          <td>{value.symbol}</td>
-                          <td>{value.name}</td>
-                          <td>{Number(value.supply).toFixed(2)}</td>
-                          <td
-                            style={
-                              value.changePercent24Hr &&
-                              value.changePercent24Hr[0] === "-"
-                                ? { backgroundColor: "#ff00004d" }
-                                : { backgroundColor: "#04760473" }
-                            }
-                          >
-                            {value.changePercent24Hr}
-                          </td>
-                          <td>{value.priceUsd}</td>
-                          <td
-                            className={styles.plus}
-                            onClick={(e) => {
-                              e.stopPropagation();
-                              setActiveCurrencyItem(value);
-                            }}
-                          >
-                            +
-                          </td>
-                        </tr>
-                      ))}
-                </tbody>
-              </table>
-              <Modal
-                active={!!activeCurrencyItem}
-                setActive={() => setActiveCurrencyItem(null)}
-              >
-                {activeCurrencyItem && (
-                  <PorfolioUpdateModalContent
-                    currencyItem={activeCurrencyItem}
-                    setActive={() => setActiveCurrencyItem(null)}
-                  />
-                )}
-              </Modal>
-            </div>
+                          {value.changePercent24Hr}
+                        </td>
+                        <td>{Number(value.priceUsd).toFixed(6)}</td>
+                        <td
+                          className={styles.plus}
+                          onClick={(e) => {
+                            e.stopPropagation();
+                            setActiveCurrencyItem(value);
+                          }}
+                        >
+                          +
+                        </td>
+                      </tr>
+                    ))}
+              </tbody>
+            </table>
+            <Modal
+              active={!!activeCurrencyItem}
+              setActive={() => setActiveCurrencyItem(null)}
+            >
+              {activeCurrencyItem && (
+                <PorfolioUpdateModalContent
+                  currencyItem={activeCurrencyItem}
+                  setActive={() => setActiveCurrencyItem(null)}
+                />
+              )}
+            </Modal>
           </>
         )}
       </div>
